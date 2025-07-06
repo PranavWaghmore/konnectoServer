169f0bf384a1ca02133e5dbc10749ee1e86e5ac1
Pranav Dipak Waghmore <pranavwaghmore1835@gmail.com>
2025-07-06 09:26:36 +0530

    Refactored routes and added services

--- src/main/kotlin/plugins/Routing.kt
+++ src/main/kotlin/plugins/Routing.kt
@@ -8,24 +8,28 @@ import pw.coding.data.repository.post.PostRepository
 import pw.coding.data.repository.user.UserRepository
 import pw.coding.routes.*
 import pw.coding.service.FollowService
+import pw.coding.service.PostService
 import pw.coding.service.UserService
 
 fun Application.configureRouting() {
     val userRepository: UserRepository by inject()
     val userService: UserService by inject()
+
     val followRepository : FollowRepository by inject()
     val followService: FollowService by inject()
+
     val postRepository : PostRepository by inject()
+    val postService: PostService by inject()
     routing {
         // User routes
         createUserRoute(userService)
-        loginUser(userRepository = userRepository)
+        loginUser(userService)
 
         // Following routes
         followUser(followService)
         unfollowUser(followService)
 
         // Post
-        createPostRoute(postRepository)
+        createPostRoute(postService)
     }
 }
