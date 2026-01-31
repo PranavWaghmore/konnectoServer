aea6199c87c155128e6159f82415d658d694bda5
Pranav Dipak Waghmore <pranavwaghmore1835@gmail.com>
2026-01-31 17:21:06 +0530

    Refactored follow and unfollow routes

--- src/main/kotlin/util/ApiResponseMessages.kt
+++ src/main/kotlin/util/ApiResponseMessages.kt
@@ -3,7 +3,7 @@ package pw.coding.util
 object ApiResponseMessages {
     const val USER_ALREADY_EXISTS = "A User with this Email-id already exists."
     const val USER_NOT_FOUND = "Sorry couldn't found User"
-    const val INVALID_CREDENTIALS = "Oops that  is not correct, please try again, "
-    const val FIELDS_BLANK = "Field cannot be empty."
+    const val INVALID_CREDENTIALS = "Oops that  is not correct, please try again.."
+    const val FIELDS_BLANK = "Fields may not be empty."
     const val COMMENT_TOO_LONG = "The comment length is exceed ${Constants.MAX_COMMENT_LENGTH} characters"
 }
